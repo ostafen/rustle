fc2f2dcf83cfd84aded03da7e72800daea1ec858
Stefano <stefano.scafiti96@gmail.com>
2022-03-15 17:16:45 +0100

    Move Message and related methods to new file

--- core/stream.go
+++ core/stream.go
@@ -2,26 +2,7 @@ package core
 
 import (
 	"log"
-	"time"
-
-	uuid "github.com/satori/go.uuid"
 )
-
-type Message struct {
-	Id        string
-	Timestamp uint64      `json:"timestamp"`
-	Stream    string      `json:"stream"`
-	Data      interface{} `json:"data"`
-}
-
-func NewMessage(stream string, data interface{}) *Message {
-	return &Message{
-		Id:        uuid.NewV4().String(),
-		Timestamp: uint64(time.Now().UnixNano()),
-		Stream:    stream,
-		Data:      data,
-	}
-}
 
 type streamConsumerGroup struct {
 	consumers    []*consumer
