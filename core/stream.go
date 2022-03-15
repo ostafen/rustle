905ce154dd9d935515c1745accf25dc5f3328149
Stefano <stefano.scafiti96@gmail.com>
2022-03-15 16:41:41 +0100

    Generate random message id

--- core/stream.go
+++ core/stream.go
@@ -1,8 +1,13 @@
 package core
 
-import "time"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type Message struct {
+	Id        string
 	Timestamp uint64      `json:"timestamp"`
 	Stream    string      `json:"stream"`
 	Data      interface{} `json:"data"`
@@ -10,6 +15,7 @@ type Message struct {
 
 func NewMessage(stream string, data interface{}) *Message {
 	return &Message{
+		Id:        uuid.NewV4().String(),
 		Timestamp: uint64(time.Now().UnixNano()),
 		Stream:    stream,
 		Data:      data,
